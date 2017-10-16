6430812540b30e94358938886a76ac98c6783078
Jim Haff <Jimmyhaff3@gmail.com>
2017-10-16 12:24:00 -0500

    added the user login and register

--- controllers/user.js
+++ controllers/user.js
@@ -5,34 +5,78 @@ const bcrypt = require('bcrypt');
 
 
 router.get('/', (req, res) => {
-  res.render('register', {})
+  res.render('register', {message: ''})
 })
 
 router.post('/register', (req, res) => {
   console.log(req.body)
 
-  const password = req.body.password;
-  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-  // create object to put into database
-  const userDBentry = {};
-  userDBentry.username = req.body.username;
-  userDBentry.password = passwordHash;
 
-  User.create(userDBentry, (err, user) => {
+
+  User.findOne({username: req.body.username}, (err, user) => {
     if(err){
-      res.send('error creating user')
+      res.send(err)
     } else {
 
-      res.send(user)
+      if(!user){
+          const password = req.body.password;
+          const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+          // create object to put into database
+          const userDBentry = {};
+          userDBentry.username = req.body.username;
+          userDBentry.password = passwordHash;
+
+          User.create(userDBentry, (err, user) => {
+            if(err){
+              res.send('error creating user')
+            } else {
+
+              res.send(user)
+            }
+          })
+      } else {
+        res.render('register', {message: 'username taken'})
+      }
     }
   })
 })
 
+router.get('/profile', (req, res) => {
+  res.send('THis is the profile page')
+})
+
 
 // Create a login view
+router.get('/login', (req, res) => {
+  res.render('login', {})
+})
 // create a login route
+router.post('/login', (req, res) => {
+  console.log(req.body)
 
+  User.findOne({username: req.body.username}, (err, user) => {
+    if(err){
+      res.send(err)
+    } else {
+      console.log(user)
+            if(user){
+
+                    if(bcrypt.compareSync(req.body.password, user.password)){
+                      res.redirect('/users/profile')
+                    } else {
+                      res.send('Login was incorrect')
+                    }
+
+            } else {
+              res.send('Login was incorrect')
+            }
+    }
+  })
+
+
+
+})
 // Use the bcrypt.compareSync('plain text password', 'hash')
 
 
